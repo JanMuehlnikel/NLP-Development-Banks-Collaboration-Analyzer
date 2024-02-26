078b7fbcfa0f5db4612a6e68c4c4ae104dd8e2ed
Jan Mühlnikel <jan.muehlnikel@gizonline.onmicrosoft.com>
2024-02-26 14:12:08 +0100

    updated iati.ipynb and deleted csv export

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,3 +1,7 @@
+"""
+CONSTANTS.py defines variables that are needed throughout this application
+"""
+
 # ORGANISATIONS defines a dict with all Organizations in this Project with its abbreviation and  IATI Code
 
 ORGANIZATIONS = {
@@ -10,3 +14,27 @@ ORGANIZATIONS = {
     "WBTF": ["World Bank Trust Funds", "wbtf", "XI-IATI-WBTF"],
     "GIZ-NON-BMZ": ["Non BMZ Activity", "giz-non-bmz", "XM-DAC-5-52"]
 }
+
+# IATI_ATTRIBUTES contains all relevant IATI Attributes that have to be fetched
+
+IATI_ATTRIBUTES = [
+    "iati_identifier",
+    "title_narrative",
+    "title_narrative_xml_lang",
+    "reporting_org_narrative",
+    "reporting_org_ref",
+    "contact_info_organisation_narrative",
+    "contact_info_department_narrative",
+    "description_narrative_xml_lang",
+    "description_narrative",
+    "recipient_country_code",
+    "recipient_region_code",
+    "location_name_narrative",
+    "activity_status_code",
+    "document_link_url",
+    "last_updated_datetime",
+    "activity_date_type",
+    "activity_date_iso_date",
+    "sector_code",
+    "sector_vocabulary",
+]
