5c7c58c325ee37898c512f7a0e404206a6708f33
Jan Mühlnikel <jan.muehlnikel@gizonline.onmicrosoft.com>
2024-02-26 15:59:36 +0100

    created fetch_api.py and added api function to pipeline

---
+++ data/pipeline/fetch_api.py
@@ -0,0 +1,59 @@
+"""
+Method to acces IATI Datastore API and store data as json
+"""
+
+import requests
+import json
+
+def fetch_data(api_key:str, code:str, abbreviation:str, fl_str:str):
+    url = "https://api.iatistandard.org/datastore/activity/select"
+
+    # Access the IATI datastore api
+    query = {
+        "q": f'reporting_org_ref:("{code}")',
+        "start": 0, 
+        "rows": 1000,  # max 1000 rows per page
+        "fl": fl_str
+    }
+
+    # set header with api key from /config/KEYS/IATI_KEY
+    # gitignore for KEY; need to create IATI Datastore API Key on your own on IATI web page
+    headers = {
+        "Ocp-Apim-Subscription-Key": api_key
+    }
+
+    try:
+        all_docs = []
+        page = 0
+
+        max_pages = 1
+        max_pages_avl = False
+        cur_page = -1
+
+        while True:
+            if cur_page < max_pages or max_pages_avl == False:
+                cur_page += 1
+                response = requests.get(url, params=query, headers=headers)
+                
+                if response.status_code == 200:
+                    data = response.json()
+                    docs = data.get("response", {}).get("docs", [])
+                    if docs:
+                        all_docs.extend(docs)
+                        query["start"] += len(docs)
+                        page += 1
+                    else:
+                        break
+                else:
+                    print("Error:", response.status_code)
+                    break
+            else:
+                break
+
+        with open(f"../../src/responses/response_{abbreviation}.json", mode='w', encoding='utf-8') as file:
+            json.dump(all_docs, file, ensure_ascii=False, indent=4)
+            
+        print(f"⯈ {cur_page} page(s) of data from {abbreviation} stored in src/responses/!")
+
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
---
+++ data/pipeline/pipeline.py
@@ -0,0 +1,48 @@
+"""
+This file contains a pipeline which can be used to extract all organizational data and transfrom it into a data frame
+
+1. Get data from IATI datastore API
+2. Save orga json in src/responses/
+3. Transofrom json file into data frame format and engineer features
+4. Export transformed orga data as csv into src/transformed
+"""
+
+###############
+# Dependecies #
+###############
+from fetch_api import fetch_data
+
+# Your current dirctory has to be /data/pipeline/ to not get FileNotFoundError
+from importlib.machinery import SourceFileLoader
+CONSTANTS = SourceFileLoader("CONSTANTS", "../../config/CONSTANTS.py").load_module()
+KEYS = SourceFileLoader("KEYS", "../../config/KEYS.py").load_module()
+
+
+# Iterate through all organizations
+
+for key, org_values in CONSTANTS.ORGANIZATIONS.items():
+    print(f"⯈ Start fetching {key}.")
+
+    #################################
+    # Fetch data with datastore API #
+    #################################
+    
+    # get features to fetch
+    fl_list = CONSTANTS.IATI_ATTRIBUTES
+    fl_str = ",".join(fl_list)   
+
+    # abbiviation gets filename
+    abbreviation = org_values[1]
+    # iati code the access IATI orga data
+    code = org_values[2]
+
+fl_list = CONSTANTS.IATI_ATTRIBUTES
+fl_str = ",".join(fl_list)   
+fetch_data(KEYS.IATI_KEY, "XM-DAC-5-52", "giz-non-bmz", fl_str)
+
+
+
+
+
+
+
