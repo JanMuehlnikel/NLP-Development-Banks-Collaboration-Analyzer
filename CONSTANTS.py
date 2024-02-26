5c4e310ad9307cfc3492640da2a30da485ee4dc6
Jan Mühlnikel <jan.muehlnikel@gizonline.onmicrosoft.com>
2024-02-26 14:01:51 +0100

    added constant py with organization dict

---
+++ CONSTANTS.py
@@ -0,0 +1,12 @@
+# ORGANISATIONS defines a dict with all Organizations in this Project with its abbreviation and  IATI Code
+
+ORGANIZATIONS = {
+    "BMZ": ["Bundesministerium für wirtschaftliche Zusammenarbeit und Entwicklung", "bmz", "DE-1"],
+    "IAD": ["Inter-American Development Bank", "iad", "XI-IATI-IADB"],
+    "ADB": ["Asian Development Bank", "adb", "XM-DAC-46004"],
+    "AfDB": ["African Development Bank", "afdb", "XM-DAC-46002"],
+    "EIB": ["European Investment Bank", "eib", "XM-DAC-918-3"],
+    "WB": ["The World Bank", "wb", "44000"],
+    "WBTF": ["World Bank Trust Funds", "wbtf", "XI-IATI-WBTF"],
+    "GIZ-NON-BMZ": ["Non BMZ Activity", "giz-non-bmz", "XM-DAC-5-52"]
+}
