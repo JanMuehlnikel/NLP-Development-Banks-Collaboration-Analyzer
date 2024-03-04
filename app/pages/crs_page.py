ffad87768ad85890a07437449730c6eb1bab5875
Jan Mühlnikel <jan.muehlnikel@gizonline.onmicrosoft.com>
2024-03-04 21:53:46 +0100

    added multi page stm

--- app/pages/crs_page.py
+++ app/pages/crs_page.py
@@ -1,8 +1,4 @@
-import streamlit as st
-from menu import menu_with_redirect
-
-# Redirect to app.py if not logged in, otherwise show the navigation menu
-menu_with_redirect()
-
-st.title("This page is available to all users")
-st.markdown(f"You are currently logged with the role of {st.session_state.role}.")
+import streamlit as stm 
+  
+stm.title("This is PageOne Geeks.") 
+stm.sidebar.success("You are currently viewing Page One Geek")
