7744c141055428da500252cc3ff4a42bc9bf63f3
Jan Mühlnikel <jan.muehlnikel@gmx.de>
2024-03-11 15:56:50 +0100

    added crs app file

---
+++ data/pipeline/app_files.py
@@ -0,0 +1,16 @@
+"""
+Create the app files 
+Need to have not one big file but multiple smaller ones to stay under git 100gb limit
+"""
+
+# Dependencies
+import pandas as pd
+
+# Load merged orgas df
+df = pd.read_csv("..\..\..\src\merged_orgas_.csv")
+
+def create_files():
+
+    # CREATE CRS DF
+    crs_df = df[["iati_id", "orga_abbreviation", "orga_full_name", "title_main", "country", "description_main", "status", "crs_5_code", "crs_3_code"]]
+    crs_df.to_csv("../../app/src/crs_data.csv", index=False)
