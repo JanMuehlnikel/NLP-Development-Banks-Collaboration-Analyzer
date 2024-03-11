0e3c06341c42486204c7012e955b67ce214d6e24
Jan Mühlnikel <jan.muehlnikel@gmx.de>
2024-03-11 16:18:24 +0100

    change app_src to src name

--- data/pipeline/app_files.py
+++ data/pipeline/app_files.py
@@ -7,10 +7,10 @@ Need to have not one big file but multiple smaller ones to stay under git 100gb 
 import pandas as pd
 
 # Load merged orgas df
-df = pd.read_csv("..\..\..\src\merged_orgas_.csv")
+df = pd.read_csv("..\..\..\src\merged_orgas.csv")
 
 def create_files():
 
     # CREATE CRS DF
     crs_df = df[["iati_id", "orga_abbreviation", "orga_full_name", "title_main", "country", "description_main", "status", "crs_5_code", "crs_3_code"]]
-    crs_df.to_csv("../../app/app_src/crs_data.csv", index=False)
+    crs_df.to_csv("../../app/src/crs_data.csv", index=False)
