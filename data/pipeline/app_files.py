1b97f90b09916c20c8579d2b7a3e6d7caf51c727
Jan Mühlnikel <jan.muehlnikel@gmx.de>
2024-03-16 15:20:47 +0100

    adjusted add_files in pipeline to create file modules for app

--- data/pipeline/app_files.py
+++ data/pipeline/app_files.py
@@ -11,12 +11,24 @@ df = pd.read_csv("..\..\src\merged_orgas.csv")
 
 def create_files():
 
-    # CREATE CRS DF
-    crs_df = df[["iati_id", "orga_abbreviation", "orga_full_name", "client", "title_main", "country", "description_main", "status", "crs_5_code", "crs_3_code"]]
-    crs_df.to_csv("../../app/src/crs_data.csv", index=False)
+    # ORGA Infos DF
+    orgas_df = df[["iati_id", "iati_orga_id", "orga_abbreviation", "client","orga_full_name"]]
+    orgas_df.to_csv("../../../app/src/projects/project_orgas.csv", index=False)
 
-    # CREATE SDG DF
-    sdg_df = df[["iati_id", "iati_orga_id", "orga_abbreviation", "orga_full_name", "client", "title_main", "country", "description_main", "status", "sgd_pred_code"]]
-    sdg_df['sgd_pred_code'] = sdg_df['sgd_pred_code'].astype('Int64')
+    # Project Text (description & title)
+    texts_df = df[["iati_id", "title_main", "description_main"]]
+    texts_df.to_csv("../../../app/src/projects/project_texts.csv", index=False)
 
-    sdg_df.to_csv("../../app/src/sdg_data.csv", index=False)
+    # Project Status DF
+    status_df = df[["iati_id", "status", "planned_start", "actual_start","planned_end", "actual_end"]]
+    status_df.to_csv("../../../app/src/projects/project_status.csv", index=False)
+
+    # Project region DF
+    region_df = df[["iati_id", "region", "location"]]
+    region_df.to_csv("../../../app/src/projects/project_region.csv", index=False)
+
+    # Project Sector DF
+    sector_df = df[["iati_id", "crs_5_code", "crs_5_name", "crs_3_code", "crs_3_name", "sgd_pred_code", "sgd_pred_str"]]
+    sector_df['sgd_pred_code'] = sector_df['sgd_pred_code'].astype('Int64')
+    sector_df.to_csv("../../../app/src/projects/project_sector.csv", index=False)
+
