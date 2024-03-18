efcd208d06acd68dcc1f289306a7d6508bd62883
Jan Mühlnikel <jan.muehlnikel@gmx.de>
2024-03-18 12:33:59 +0100

    test change for st

--- app/similarity.py
+++ app/similarity.py
@@ -12,7 +12,6 @@ import utils.similarity_table as similarity_table
 
 # Catch DATA
 # Load Similarity matrix
-@st.cache_data
 def load_sim_matrix():
     loaded_matrix = load_npz("app/src/similarities.npz")
     dense_matrix = loaded_matrix.toarray()
